a5102aea03a808d2218d0a38b47f86ce556dd346
bharatayasa <bharatayasa40@gmail.com>
2024-10-30 19:07:05 +0800

    ad data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,44 @@
 import { Response } from 'express';
 import { UsersService } from './users.service';
-import { Role } from '@prisma/client';
 import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res, UseGuards } from '@nestjs/common';
 import * as moment from 'moment';
 import { AuthGuard } from '../auth/guard/auth.guard';
 import { Roles } from '../auth/guard/roles.decorator';
+import { UserDto } from './userDTO';
 
 @Controller('users')
 export class UsersController {
-    constructor(){}
+    constructor(private readonly usersService: UsersService) {}
 
     @Get()
     @Roles('admin')
     @UseGuards(AuthGuard)
     async getAllUsers(@Res() res: Response) {
         try {
+            const users = await this.usersService.findAll()
+
+            if (!users || users.length === 0) {
+                return res.status(HttpStatus.NOT_FOUND).json({
+                    message: "data not found", 
+                    data: users
+                })
+            }
+
+            const formatData = users.map(user => ({
+                username : user.username, 
+                name     : user.name,
+                email    : user.email,
+                role     : user.role, 
+                createdAt: moment(user.createdAt).format('YYYY-MM-DD'),
+            }))
+
+            console.log(formatData);
+
             return res.status(HttpStatus.OK).json({
-                message: "yeee succes to get by admin"
+                message: "Success to get data by admin",
+                data: formatData
             })
+
         } catch (error) {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
                 message: 'Failed to get all data',
@@ -26,4 +47,65 @@ export class UsersController {
         }
     }
 
+    @Get(':id')
+    @Roles('admin')
+    @UseGuards(AuthGuard)
+    async getUserById(@Param('id') id: number, @Res() res: Response){
+        try {
+            const users = await this.usersService.findOne(Number(id));
+
+            if (!users) {
+                return res.status(HttpStatus.NOT_FOUND).json({
+                    message: 'data not found',
+                    data: users
+                });
+            }
+
+            const formatData = users.map(user => ({
+                username : user.username, 
+                name     : user.name,
+                email    : user.email,
+                role     : user.role, 
+                createdAt: moment(user.createdAt).format('YYYY-MM-DD'),
+            }))
+
+            return res.status(HttpStatus.OK).json({
+                message: "success to get data by id", 
+                data: formatData
+            })
+
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+                message: 'Failed to get data by id',
+                error: error.message,
+            });
+        }
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    @Roles('admin')
+    async addUser(@Body() body: UserDto, @Res() res: Response){
+        try {
+            const newUser = await this.usersService.createUser(body)
+
+            const formData = {
+                username : newUser.username, 
+                name     : newUser.name, 
+                email    : newUser.email, 
+                role     : newUser.role
+            }
+
+            return res.status(HttpStatus.CREATED).json({
+                message: "Successful to create user", 
+                data: formData
+            })
+
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+                message: 'Failed to create data',
+                error: error.message,
+            });
+        }
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { UserDto } from './userDTO';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {} 
+
+    async findAll(): Promise<User[]> {
+        return this.prisma.user.findMany();
+    }
+
+    async findOne(id: number): Promise<User[]> {
+        return this.prisma.user.findMany({
+            where: {
+                id
+            }
+        });
+    }
+
+    async createUser(data: UserDto): Promise<User> {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+    
+        const newUser = await this.prisma.user.create({
+            data: {
+                username: data.username,
+                name: data.name,
+                email: data.email,
+                password: hashedPassword,
+                role: data.role,
+            },
+        });
+    
+        return newUser;
+    }
+}
+
